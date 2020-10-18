ac63054d51cc63ae7f7c210d3ca563118262805a
Akhilesh Mourya <mouryaakhilesh62@gmail.com>
2020-10-18 22:34:31 +0530

    Added a React Native example

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
 import { NativeModules } from 'react-native';
 
-const { RNLoginWithAmazon } = NativeModules;
-const LoginWithAmazon = RNLoginWithAmazon
+const { RNAmazonAuthentication } = NativeModules;
+const LoginWithAmazon = RNAmazonAuthentication
 
 export default LoginWithAmazon;
